53671fbec8bf4bc4df6376e033dad90f6de498c0
Yash Patel <patelymp007@gmail.com>
2020-12-02 22:27:53 -0800

    project start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BudgetChart from './Components/BudgetChart';
+
+function App() {
+  return (
+    <div className="App">
+      <BudgetChart/>
+    </div>
+  );
+}
+
+export default App;
